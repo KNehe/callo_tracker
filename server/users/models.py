2dbbe0dd7245cf4232bf9b20e678cdc8db961f09
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2024-03-06 14:18:26 +0300

    create custom user manager

--- server/users/models.py
+++ server/users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from users.managers import CustomUserManager
+
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ['password', "username"]
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
         return self.email
