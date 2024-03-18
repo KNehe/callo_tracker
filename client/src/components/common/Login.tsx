3df35ecec1759db429f38968a6d423377c5cc818
DreMukare <dremukare@gmail.com>
2024-03-18 15:55:06 +0300

    minor: controlled inputs for email and password

--- client/src/components/common/Login.tsx
+++ client/src/components/common/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
 	Button,
 	Container,
@@ -9,14 +10,27 @@ import {
 import LSLayout from './Login-SignUp-Layout';
 
 const Login = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
 		<LSLayout>
 			<Title order={2} ta={'left'}>
 				Login
 			</Title>
 			<Stack gap="1em">
-				<TextInput label="Email" placeholder="Enter your email" />
-				<PasswordInput label="Password" placeholder="Enter your password" />
+				<TextInput
+					label="Email"
+					placeholder="Enter your email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<PasswordInput
+					label="Password"
+					placeholder="Enter your password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 				<Button
 					onClick={() => {
 						// make API request to log user in
